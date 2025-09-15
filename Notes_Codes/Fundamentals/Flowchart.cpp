0143b29c296ddea0efc1d81679d2fda6a8a8fc05
CrazyCoders001 <akasgkanni45@gmail.com>
2025-09-15 21:08:33 +0530

    Create Flowchart.cpp

---
+++ Notes_Codes/Fundamentals/Flowchart.cpp
@@ -0,0 +1,32 @@
+// learn the structure of this code
+
+// The aim here is to learn concepts and logic building for placements
+#include <bits/stdc++.h>   //This line is import library line
+#include <iostream>
+
+using namespace std;    //This is adding a std namespace
+
+int main()        //IN java, c, cpp, python the code execution starts from main function 
+{
+    cout<<"Helloc World"<<endl;
+    //cout :- inbuild function (it prints data on the screen)
+    //endl :- it is used to print a new line
+    //return 0; :- it is used to return a value to the operating system
+
+
+    // Flow chart :- Flow of execution of the code
+    // oval :- start or end
+    // rectangle :- operation
+    // diamond :- condition + decision making
+    // parallelogram :- input/output
+    // arrow :- flow
+
+    int a = 10;     
+    int b = 20;
+    // we are taking inputs    :- parallelogram
+    int sum = a + b;   // we are performing operation :- rectangle
+    cout<<sum<<endl;   // we are printing the output :- parallelogram
+
+
+    return 0;
+}
