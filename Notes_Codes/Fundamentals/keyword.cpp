0270dfe250da38c3dd9101966a538c62fced9f17
CrazyCoders001 <akasgkanni45@gmail.com>
2025-09-17 18:33:20 +0530

    Create keyword.cpp

---
+++ Notes_Codes/Fundamentals/keyword.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // What are keywords ?    
+    // keywords are also known as data types  :- wrong statement 
+
+    // Keyword :- Keywords are predefined and reserved words in cpp 
+    // Each keyword has a specific meaning and purpose in the language  
+
+    // int, float, char, double, bool  :- it fits right 
+    // for, return, if, else, switch, case, break, continue, goto etc.. :- it fits wrong
+
+    //can i create a integer variable named as char ? :- No because char is a keyword 
+    char my_char = 'a'; 
+    cout<<my_char<<endl;
+
+    // Link to keyword list :- https://www.geeksforgeeks.org/cpp/cpp-keywords/ 
+
+    return 0;
+}
